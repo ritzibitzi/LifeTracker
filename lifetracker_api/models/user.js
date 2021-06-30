721a379bc192f248d9c836624b8295c17c6e9c1d
mpadil18 <mpadil18@ucsc.edu>
2021-06-30 11:26:22 -0700

    Update the User model: Register Users Method

--- lifetracker_api/models/user.js
+++ lifetracker_api/models/user.js
@@ -1,5 +1,5 @@
 const db = require("../db")
-const { UnauthorizedError } = require("../utils/errors");
+const { UnauthorizedError, BadRequestError } = require("../utils/errors");
 
 class User {
     static async login(credentials) {
@@ -17,14 +17,68 @@ class User {
     static async register(credentials) {
         //User should submit email, pw, [RSVP STATUS], and [NUM OF GUESTS]
         //If any missing, throw an error.
+        const requiredFields = ["email", "username", "first_name", "last_name", "password"];
+        requiredFields.forEach(field => {
+            if (!credentials.hasOwnProperty(field)) {
+                throw new BadRequestError(`Missing ${field} in request body.`)
+            }
+        })
 
-        //Ensure user doesn't already exist in the system w/ the email
-        //If email exists in db, throw error
-
+        if(credentials.email.indexOf("@") <= 0 ) {
+            throw new BadRequestError("Invalid email.");
+        }
+        //Ensure user doesn't already exist in the system w/ the email OR username.
+        //If email OR username exists in db, throw error
+        const existingEmail = await User.fetchUserByField(credentials.email, "email")
+        const existingUsername = await User.fetchUserByField(credentials.username, "username")
+        if (existingEmail) {
+            throw new BadRequestError(`Duplicate email: ${credentials.email}`)
+        } 
+        
+        if (existingUsername) {   
+            throw new BadRequestError(`This username already exists: ${credentials.username}`)
+        }
         //Take user's pw and hash it.
         //Take user's email and lowercase it.
+        const lowerCasedEmail = credentials.email.toLowerCase();
+        const lowerCasedUsername = credentials.username.toLowerCase();
 
         //Create a new user in the db with all their info
         //return the user.
+        const result = await db.query(`
+        INSERT INTO users (password, username, first_name, last_name, email)
+        VALUES ($1, $2, $3, $4, $5)
+        RETURNING id, username, email, first_name AS "firstName", last_name AS "lastName", created_at;
+        `,
+        [
+            /*lowerCasedUsername,
+            password,
+            firstName,
+            lastName,
+            lowerCasedEmail,
+            created_at*/
+
+            credentials.password,
+            lowerCasedUsername,
+            credentials.first_name,
+            credentials.last_name,
+            lowerCasedEmail
+        ]
+        )
+
+        const user = result.rows[0];
+        return user;
+    }
+
+    static async fetchUserByField(fieldVal, fieldName) {
+        if (!fieldVal && !(fieldName)) {
+            throw new BadRequestError(`No ${fieldName} provided`);
+        }
+        const query = `SELECT * FROM users WHERE ${fieldName} = $1`;
+        const result = await db.query(query, [fieldVal.toLowerCase()]);
+        const user = result.rows[0];
+        return user;
     }
 }
+
+module.exports = User;
--- lifetracker_api/routes/auth.js
+++ lifetracker_api/routes/auth.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const User = require("../models/user");
 const router = express.Router();
-const User = require("../models/user")
 
 router.post("/login", async (req, res, next) => {
     try {
         //Take user email and password. Attempt to authenticate them.
+        const user = await User.login(req.body);
+        return res.status(200).json({ user });
     } catch(err) {
         next(err);
     }
@@ -14,6 +16,8 @@ router.post("/register", async (req, res, next) => {
     try {
         //Take user email, password, [RSVP STATUS], and [THE NUMBER OF GUESTS]
         //Create a new user in our database.
+        const user = await User.register(req.body);
+        return res.status(201).json({ user });
     } catch(err) {
         next(err);
     }
