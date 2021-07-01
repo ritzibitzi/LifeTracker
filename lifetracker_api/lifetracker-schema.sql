3f9b59d05f7bbccc3fa09c2cebd354b66bd7b3d6
mpadil18 <mpadil18@ucsc.edu>
2021-07-01 10:41:14 -0700

    Database shema setup

--- lifetracker_api/lifetracker-schema.sql
+++ lifetracker_api/lifetracker-schema.sql
@@ -7,3 +7,25 @@ CREATE TABLE users (
     email       TEXT NOT NULL UNIQUE CHECK (POSITION('@' IN email) > 1),
     created_at  TIMESTAMP NOT NULL DEFAULT NOW()
 );
+
+CREATE TABLE loglines (
+    id          SERIAL PRIMARY KEY,
+    user_id     INTEGER NOT NULL,
+    title       TEXT NOT NULL,
+    protagonist TEXT NOT NULL,
+    incident    TEXT NOT NULL,
+    goal        TEXT NOT NULL,
+    conflict    TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE ideas (
+    id          SERIAL PRIMARY KEY,
+    user_id     INTEGER NOT NULL,
+    project     TEXT NOT NULL,
+    title       TEXT NOT NULL UNIQUE,
+    category    TEXT NOT NULL,
+    info        TEXT NOT NULL,
+    resources   TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
